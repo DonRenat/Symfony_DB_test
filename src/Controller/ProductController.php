6b3d63617d1bc9424a9019e6e449ef04131b9429
ren99999@mail.ru <GasanRen123>
2019-10-28 12:24:31 +1000

    Working version 2. Minor updates and clean-up.

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -18,33 +18,6 @@ use Doctrine\ORM\Query;
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/product", name="create_product")
-     */
-    public function createProduct(ValidatorInterface $validator): Response
-    {
-        // you can fetch the EntityManager via $this->getDoctrine()
-        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
-        $entityManager = $this->getDoctrine()->getManager();
-
-        $product = new Product();
-        $product->setName('Monitor');
-        $product->setPrice(19999);
-        $product->setDescription('Full HD, 144Hz');
-
-        $errors = $validator->validate($product);
-        if (count($errors) > 0) {
-            return new Response((string)$errors, 400);
-        }
-
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($product);
-
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
-
-        return new Response('Saved new product with id ' . $product->getId());
-    }
-    /**
      * @Route("/product/{id}", name="product_show")
      */
     public function show($id)
@@ -60,61 +33,6 @@ class ProductController extends AbstractController
         }
 
         return new Response('Check out this great product: '.$product->getName());
-    }
-    /**
-     * @Route("/product/edit/{id}")
-     */
-    public function update($id)
-    {
-        $entityManager = $this->getDoctrine()->getManager();
-        $product = $entityManager->getRepository(Product::class)->find($id);
-
-        if (!$product) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
-        }
-
-        $product->setName('Intel Core i7 7700K');
-        $entityManager->flush();
-
-        return $this->redirectToRoute('product_show', [
-            'id' => $product->getId()
-        ]);
-    }
-    /**
-     * @Route("/product/delete/{id}")
-     */
-    public function delete($id)
-    {
-        $entityManager = $this->getDoctrine()->getManager();
-        $product = $entityManager->getRepository(Product::class)->find($id);
-
-        if (!$product) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
-        }
-
-        $entityManager->remove($product);
-        $entityManager->flush();
-
-        return new Response('Deleted');
-    }
-    /**
-     * @Route("/productAll", name="product_show_1")
-     */
-    public function showAll()
-    {
-        $query = $this->getDoctrine()
-            ->getRepository(Product::class)
-            ->createQueryBuilder('c')
-            ->getQuery();
-        $result = $query->getResult(Query::HYDRATE_ARRAY);
-
-        return new Response(
-            '<html><body>All products: '.$result[1]['name'].'</body></html>'
-        );
     }
     //test
     /**
